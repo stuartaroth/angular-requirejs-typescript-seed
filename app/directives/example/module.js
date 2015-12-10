d22749a9c42c3cd6ed0dbda3f72f68c5c4691316
SRoth <SRoth@dealersocket.com>
2015-12-10 11:46:57 -0700

    Removed js

--- app/directives/example/module.js
+++
@@ -1,7 +0,0 @@
-///<reference path="../../../typings/tsd.d.ts"/>
-define(["require", "exports", 'angular', './directive', './controller'], function (require, exports, angular, exampleDirective, exampleController) {
-    return angular.module('myApp.example', [])
-        .controller('myAppExampleController', exampleController)
-        .directive('myAppExample', exampleDirective.create);
-});
-//# sourceMappingURL=module.js.map
